1a59faaf1da3bd6f348fd5f944be2cd69d736168
Ailton Santana <ppsilvasantana@gmail.com>
2024-02-19 18:46:00 -0300

    primeira página

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+export { useState } from 'react';
+
+function Home() {
+    return<div>
+       <h1>Home</h1>
+       <Contador />
+    </div>
+
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1);
+
+    function adicionarContador(){
+        setContador(contador+1);
+    }
+    function subtrairContador(){
+        setContador(contador-1);
+    }
+
+    return(
+        <div>
+        <div>{contador}</div>
+        <button onClick={adicionarContador}>Adicionar</button>
+        <p>-------------</p>
+        <button onClick={subtrairContador}>Remover</button>
+        </div>
+    )
+        
+}
+
+export default Home
