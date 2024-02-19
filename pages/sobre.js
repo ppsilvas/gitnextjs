4bd8f8d94d1e2626c967cda0aec360a970c8d3b5
Ailton Santana <ppsilvasantana@gmail.com>
2024-02-19 20:29:31 -0300

    páginas adicionais

--- pages/sobre.js
+++ pages/sobre.js
@@ -2,15 +2,15 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import Link from "next/link";
 
-function Home() {
+function Sobre() {
     return(<div>
-       <h1>Home</h1>
+       <h1>Sobre</h1>
 
-        <Link href="/sobre">
-            Acessar página Sobre
+        <Link href="/">
+            Acessar página Home
         </Link>
      </div>)
 }
 
 <SpeedInsights />
-export default Home
+export default Sobre
